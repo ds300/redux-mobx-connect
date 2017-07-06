890289aacde5d99813122e77ec09a5ecc78022d4
David Sheldrick <david@futurice.com>
2017-07-07 00:55:44 +0100

    nitial commit

---
+++ REAMDE.md
@@ -0,0 +1,114 @@
+# redux-mobx-connector
+
+    yarn add redux-mobx-connect
+
+## A simple alternative to react-redux
+
+react-redux is a remarkable piece of battle-tested engineering with a
+work-of-art API. This... is not that.
+
+This is a so-simple-it's-almost-dumb connector for redux stores which uses MobX to
+hook stuff together. The API is obvious and boring, and you'll be yawning
+all the way to the bank.
+
+This
+
+```typescript
+import {connect} from "react-redux"
+
+const ConnectedComponent = connect(
+  null,
+  (dispach, {id}) => ({onPress: () => dispatch(somethingHappened(id))})
+)(Component)
+```
+
+becomes this:
+
+```typescript
+import {connect} from "redux-mobx-connect"
+
+const ConnectedComponent = connect(store => ({id}) =>
+  <Component onPress={() => store.dispatch(somethingHappened(id))}>
+)
+```
+
+And this (from redux docs):
+
+```typescript
+const mapStateToProps = state => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTodoClick: id => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
+```
+
+becomes this:
+
+```typescript
+const VisibleTodoList = connect(
+  store =>
+    class VisibleTodoList extends React.Component {
+      onTodoClick = id => store.dispatch(toggleTodo(id))
+
+      render() {
+        const todos = getVisibleTodos(store.state.todos, store.state.visibilityFilter)
+        return (
+          <VisibleTodosList todos={todos} onTodoClick={this.onTodoClick} />
+        )
+      }
+    },
+)
+```
+
+`connect` takes a function which is passed a reactive store object, and can return either a stateless functional component or a es6 component class. So you write your connected components just like your
+regular compoents, except wrapped in a lexical context where they have access to your redux store.
+
+Yes it really is that simple. The only real gotcha is that you can't destructure the store
+outside of the of the inner class or functional component.
+
+e.g.
+
+BAD:
+
+```typescript
+const VisibleTodoList = connect(
+  ({state, dispatch}) =>
+    class VisibleTodoList extends React.Component { ... },
+)
+```
+
+GOOD:
+
+```typescript
+const VisibleTodoList = connect(
+  store => class VisibleTodoList extends React.Component { ... },
+)
+```
+
+Doing it the bad way means you lose the reactivity goodness that MobX
+sets up for you, and state changes won't be propagated from store to component.
+
+Since it is built on MobX you can
+even use `@observable` and `@computed` properties in your connected components to
+manage component local state and memoize expensive derived state. No more reselect!
+
+This is brand new so will be beta for a short while.
+
+## License
+
+MIT
+
+[![Empowered by Futurice's open source sponsorship program](https://img.shields.io/badge/sponsor-chilicorn-ff69b4.svg)](http://futurice.com/blog/sponsoring-free-time-open-source-activities?utm_source=github&utm_medium=spice&utm_campaign=redux-mobx-connector)
---
+++ index.tsx
@@ -0,0 +1,119 @@
+import * as React from "react"
+import { observable } from "mobx"
+import { observer } from "mobx-react"
+
+import { Store, Unsubscribe } from "redux"
+
+/**
+ * Provider takes a plain redux store and adds child context for an observable
+ * version of the store
+ */
+export class Provider extends React.PureComponent<{
+  store: Store<any>
+  children: React.ReactElement<any> | null
+}> {
+  static childContextTypes = {
+    observableStore: () => null,
+  }
+
+  observableStore: ObservableStore<any, any> = new ObservableStoreImpl()
+  unsubscribe: Unsubscribe = () => void 0
+
+  componentWillMount() {
+    this.componentWillReceiveProps(this.props)
+  }
+
+  componentWillReceiveProps(nextProps: { store: Store<any> }) {
+    this.unsubscribe()
+    this.observableStore.state = nextProps.store.getState()
+    this.observableStore.dispatch = nextProps.store.dispatch.bind(
+      this.props.store,
+    )
+    this.unsubscribe = nextProps.store.subscribe(() => {
+      this.observableStore.state = nextProps.store.getState()
+    })
+  }
+
+  getChildContext() {
+    return { observableStore: this.observableStore }
+  }
+
+  render() {
+    return this.props.children as React.ReactElement<any> | null
+  }
+}
+
+export interface ObservableStore<State, Action = any> {
+  state: State
+  dispatch(a: Action): void
+}
+class ObservableStoreImpl<State, Action = any> {
+  @observable.ref state: State = null as any
+  @observable.ref dispatch: (a: Action) => void = null as any
+}
+
+function hasDisplayName(val: any): val is { displayName: string } {
+  return val && "displayName" in val && typeof val.displayName === "string"
+}
+
+function getDisplayName(
+  nameOrComponent: string | { name: string } | { displayName: string },
+) {
+  return typeof nameOrComponent === "string"
+    ? nameOrComponent
+    : hasDisplayName(nameOrComponent)
+      ? nameOrComponent.displayName
+      : nameOrComponent ? nameOrComponent.name : "Connected"
+}
+
+export function connect<State, OwnProps, Action = any>(
+  factory: (
+    store: ObservableStore<State, Action>,
+  ) => React.ComponentClass<OwnProps> | React.StatelessComponent<OwnProps>,
+  displayName?: string | { name: string } | { displayName: string },
+): React.ComponentClass<OwnProps> {
+  const storeBox = observable.shallowBox(
+    new ObservableStoreImpl<State, Action>(),
+  )
+  const storeProxy = observable({
+    get state() {
+      return storeBox.get().state
+    },
+    get dispatch() {
+      return storeBox.get().dispatch
+    },
+  })
+  const Component = observer(factory(storeProxy))
+
+  const name = getDisplayName(displayName || Component) || "Connected"
+
+  class Wrapper extends React.PureComponent<OwnProps> {
+    static displayName = name
+    static contextTypes = {
+      observableStore: () => null,
+    }
+
+    context: {
+      observableStore: ObservableStore<State, Action>
+    }
+
+    componentWillMount() {
+      storeBox.set(this.context.observableStore)
+    }
+
+    render() {
+      return <Component {...this.props} />
+    }
+  }
+
+  return Wrapper
+}
+
+export function makeConnect<State, Action = any>(): <OwnProps>(
+  factory: (
+    store: ObservableStore<State, Action>,
+  ) => React.ComponentClass<OwnProps> | React.StatelessComponent<OwnProps>,
+  displayName?: string | { name: string } | { displayName: string },
+) => React.ComponentClass<OwnProps> {
+  return connect as any
+}
